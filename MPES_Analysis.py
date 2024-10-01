05b041d1d4f40bbc71e179df3f79ec53467d7798
Lawson Lloyd <51204777+lawsonlloyd@users.noreply.github.com>
2024-10-01 13:54:23 +0200

    Update MPES_Analysis.py

--- MPES_Analysis.py
+++ MPES_Analysis.py
@@ -8,10 +8,11 @@ from Main import main
 
 #%% Specifiy filename of h5 file in your path.
 
-fn = 'your_data_file.h5'
-fn = 'Scan162_binned_100x100x200x150_CrSBr_RT_750fs_New_2.h5'
+data_path = 'path_to_your_data'
+filename = 'your_data_file.h5'
 
 data_path = 'R:\Lawson\Analysis\data'
+filename = 'Scan162_binned_100x100x200x150_CrSBr_RT_750fs_New_2.h5'
 
 # Include manual energy and time delay offsets for the axes, if required.
 E_offset = -0.1
@@ -19,7 +20,7 @@ delay_offset = 100
 
 #%% Load the data and axes information
 
-data_loader = DataLoader(data_path + '\\' + fn)
+data_loader = DataLoader(data_path + '\\' + filename)
 I, ax_kx, ax_ky, ax_E, ax_delay = data_loader.load()
 
 #%% Run the Interactive GUI for Data Exploration and Plotting
