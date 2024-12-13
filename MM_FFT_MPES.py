68dba09d50e24eb6ed14cb90035c645413297e81
Lawson Lloyd <51204777+lawsonlloyd@users.noreply.github.com>
2024-12-13 17:59:47 +0100

    MM updates

--- MM_FFT_MPES.py
+++ MM_FFT_MPES.py
@@ -12,122 +12,61 @@ from matplotlib.patches import Rectangle
 from matplotlib.ticker import FormatStrFormatter
 from skimage.draw import disk
 from scipy.optimize import curve_fit
-
-#%%
-
-### Transform Data if needed....
-#I = np.transpose(I, (0,2,1,3))
-
-if I.ndim > 3:
-    t0 = data_handler.get_t0()
-    
-    I_neg = I[:,:,:,5:t0-7] #Sum over delay/polarization/theta...
-    neg_length = I_neg.shape[3]
-    I_neg = I_neg.sum(axis=(3))
-        
-    I_pos = I[:,:,:,t0+1:-3]
-    pos_length = I_pos.shape[3]
-    I_pos = I_pos.sum(axis=(3)) #Sum over delay/polarization/theta...
-    
-    I_sum = I[:,:,:,:].sum(axis=(3))    
-
-else:
-    I_neg = I[:,:,:] #Sum over delay/polarization/theta...
-    I_pos = I[:,:,:]
-    I_sum = I
-
-dkx = (ax_kx[1] - ax_kx[0])
-dE = ax_E[1] - ax_E[0]
-
-ax_E_offset = data_handler.ax_E
-ax_delay_offset = data_handler.ax_delay
-
-cmap_LTL = plot_manager.custom_colormap(plt.cm.viridis, 0.2) #choose colormap based and percentage of total map for new white transition map
-
-#%%
-%matplotlib inline
-
-###
-# Window and Symmetrize MM for FFT
-###
-
 from scipy import signal
 from scipy.fft import fft, fftshift
 
-tMaps, tint  = [1.35], 6
-k_i, k_f = -.3, .3 #ky
-k_i_2, k_f_2 = 0, 1.15 #kx
+#%%
 
-### Plot
-numPlots = len(tMaps)
+def transform_data(data_handler, I, neg_time):
+    if I.ndim > 3:
+        t0 = data_handler.get_t0()
+        
+        neg_time = neg_time
+        tnf = (np.abs(ax_delay_offset - neg_time)).argmin()
+        I_neg = I[:,:,:,5:tnf+1]
+        neg_length = I_neg.shape[3]
+        I_neg = I_neg.sum(axis=(3))/neg_length
+            
+        I_pos = I[:,:,:,t0:]
+        pos_length = I_pos.shape[3]
+        I_pos = I_pos.sum(axis=(3))/pos_length
+        
+        I_sum_delay = I[:,:,:,:].sum(axis=(3))    
+    else:
+        I_neg = I[:,:,:] #Sum over delay/polarization/theta...
+        I_pos = I[:,:,:]
+        I_sum_delay = I
 
-fig, ax = plt.subplots(1, 3, sharey=True)
-plt.gcf().set_dpi(300)
-ax = ax.flatten()
+    return I_neg, I_pos, I_sum_delay, t0
 
-sat = [1, 1, 1]
-for i in np.arange(numPlots):
-    tMap = tMaps[i]
-    tMap = (np.abs(ax_E_offset - tMap)).argmin()
+
+def make_MM_frames(I_neg, I_pos, E, E_int):
+    E_int = E_int/2
+    E_i = (np.abs(ax_E_offset - (E-Eint))).argmin()
+    E_f = (np.abs(ax_E_offset - (E+Eint))).argmin()
     
-    frame_neg = np.transpose(I_neg[:,:,tMap-int(tint/2):tMap+int(tint/2)].sum(axis=(2)))
-    frame_pos = np.transpose(I_pos[:,:,tMap-int(tint/2):tMap+int(tint/2)].sum(axis=(2)))
-    
-    frame_neg = frame_neg/((neg_length))
-    frame_pos = frame_pos/((pos_length))
+    frame_neg = np.transpose(I_neg[:,:,E_i:E_f+1].sum(axis=(2)))
+    frame_pos = np.transpose(I_pos[:,:,E_i:E_f+1].sum(axis=(2)))
     frame_sum = frame_neg + frame_pos
     
-    cts_pos = np.sum(frame_pos[:,:])
-    cts_neg = np.sum(frame_neg[:,:])
-    cts_total = np.sum(frame_sum)
-    
+    cts_pos, cts_neg, cts_total = np.sum(frame_pos[:,:]),  np.sum(frame_neg[:,:]),  np.sum(frame_sum)
     frame_diff = frame_pos - frame_neg
-    frame_diff = (frame_diff)
-    
-    ###                   ###
-    ### Do the Operations ###
-    ###                   ###  
-    
-    kspace_frame = frame_diff
-    
+
+    return frame_pos, frame_neg, frame_diff
+
+
+def window_MM(kspace_frame, k_i_x, k_f_x, k_i_y, k_f_y):    
+
     #mn = np.mean(kspace_frame[:,25:35])
     #kspace_frame = kspace_frame - mn
-    kspace_frame[kspace_frame<0] = 0
-    
-    #kspace_frame = frame_pos #All Pos delays
+    #kspace_frame[kspace_frame<0] = 0
     
     ### Deconvolve k-space momentum broadening, Gaussian with FWHM 0.063A-1
     fwhm = 0.063
     fwhm_pixel = fwhm/dkx
     sigma = fwhm_pixel/2.355
     
-    gaussian_kx = signal.gaussian(len(ax_kx), std = sigma)
-    gaussian_kx = gaussian_kx/np.max(gaussian_kx)
-    gaussian_ky = signal.gaussian(len(ax_ky), std = sigma)
-    gaussian_ky = gaussian_ky/np.max(gaussian_ky)
-    
     gaussian_kxky = np.outer(gaussian_kx, gaussian_ky)
-    gaussian_kxky = gaussian_kxky/np.max(gaussian_kxky)
-    
-    #fig, ax = plt.subplots(1, 3)
-    #ax.flatten()
-    #ax[0].imshow(kspace_frame, cmap='terrain_r')
-    #ax[0].axhline(48)
-    #ax[0].axvline(55)
-
-    #i = 48
-    #j = 55
-    #kx_cut = kspace_frame[:,j-1:j+1].sum(axis=1)
-    #kx_cut = kx_cut/np.max(kx_cut)
-    #ky_cut = kspace_frame[i-1:i+1,:].sum(axis = 0)
-    #ky_cut = ky_cut/np.max(ky_cut)
-
-    #ax[1].plot(gaussian_kx, linestyle = 'dashed', color = 'green')
-    #ax[1].plot(kx_cut)
-    #ax[2].plot(gaussian_ky, linestyle = 'dashed', color = 'green')
-    #ax[2].plot(ky_cut)
-    #ax[1].set_xlim(30,70)
-    
     #kx_cut_deconv = signal.deconvolve(kx_cut, gaussian_kx)
     
     #### Apply Symm and windows
@@ -140,10 +79,10 @@ for i in np.arange(numPlots):
     win_1_box = np.zeros(kspace_frame.shape[0])
     win_2_box = np.zeros(kspace_frame.shape[1])
 
-    k_i = (np.abs(ax_ky - k_i)).argmin()
-    k_f = (np.abs(ax_ky - k_f)).argmin()
-    k_i_2 = (np.abs(ax_kx - k_i_2)).argmin()
-    k_f_2 = (np.abs(ax_kx - k_f_2)).argmin()
+    k_i = (np.abs(ax_ky - k_i_y)).argmin()
+    k_f = (np.abs(ax_ky - k_f_y)).argmin()
+    k_i_2 = (np.abs(ax_kx - k_i_x)).argmin()
+    k_f_2 = (np.abs(ax_kx - k_f_x)).argmin()
     
     tuk_1 = signal.windows.tukey(k_f-k_i)
     box_1 = signal.windows.boxcar(k_f-k_i)
@@ -178,8 +117,39 @@ for i in np.arange(numPlots):
     windowed_frame_symm = frame_sym*window_6
     windowed_frame_nonsymm = kspace_frame*window_6
     #windowed_frame_symm = frame_sym
+
+    return windowed_frame_nonsymm
+
+#%%
+
+I_neg, I_pos, I_sum_delay, t0 = transform_data(data_handler, I, -60) # Get negative delay and positive delay data sets
+
+ax_E_offset = data_handler.ax_E
+ax_delay_offset = data_handler.ax_delay
     
+dkx = (ax_kx[1] - ax_kx[0])
+dE = ax_delay_offset[1] - ax_delay_offset[0]
+
+cmap_LTL = plot_manager.custom_colormap(plt.cm.viridis, 0.2) #choose colormap based and percentage of total map for new white transition map
     
+#%%
+%matplotlib inline
+
+###
+# Window and Symmetrize MM for FFT
+###
+
+save_figure = True
+figure_file_name = '2DFFT_Windowing' 
+    
+E, E_int  = 1.35, 0.160
+k_i_y, k_f_y = -.3, .3 #ky
+k_i_x, k_f_x = 0, 1.15 #kx
+
+fig, ax = plt.subplots(1, 3, sharey=True)
+plt.gcf().set_dpi(300)
+ax = ax.flatten()
+            
 im = ax[0].imshow(kspace_frame, origin='lower', cmap=cmap_LTL, interpolation='none', extent = [ax_kx[0],ax_kx[-1],ax_ky[0],ax_ky[-1]]) #kx, ky, t
 im = ax[1].imshow(frame_sym, origin='lower', cmap=cmap_LTL, interpolation='none', extent = [ax_kx[0],ax_kx[-1],ax_ky[0],ax_ky[-1]]) #kx, ky, t
 im = ax[2].imshow(windowed_frame_symm, origin='lower', cmap=cmap_LTL, interpolation='none', extent = [ax_kx[0],ax_kx[-1],ax_ky[0],ax_ky[-1]]) #kx, ky, t
@@ -211,8 +181,7 @@ for i in np.arange(3):
     ax[i].set_xlabel('$k_x$', fontsize = 14)
     ax[i].set_ylabel('$k_y$', fontsize = 14)
     ax[i].tick_params(axis='both', labelsize=12)
-    ax[i].set_title('$E$ = ' + str((tMaps[0])) + ' eV', fontsize = 16)
-    #ax[i].annotate(('E = '+ str(round(tMaps[i],2)) + ' eV'), xy = (-1.85, 1.6), fontsize = 14, weight = 'bold')
+    ax[i].set_title('$E$ = ' + str(E) + ' eV', fontsize = 16)
 
 fig.subplots_adjust(right=0.8)
 cbar_ax = fig.add_axes([1, 0.2, 0.025, 0.6])
@@ -222,9 +191,17 @@ fig.colorbar(im, cax=cbar_ax, ticks = [10,100])
 fig.tight_layout()
 plt.show()
 
+if save_figure is True:
+    fig.savefig((figure_file_name +'.svg'), format='svg')
+
 #%%
 
 %matplotlib inline
+
+
+save_figure = True
+figure_file_name = 'MM_FFT' 
+    
 
 #####                                              #####
 ##### Plot FFT of MMs to obtain real space wavefxn #####
@@ -317,8 +294,6 @@ im0 = ax[1].imshow(momentum_frame/np.max(momentum_frame), clim = None, origin = 
 im = ax[2].imshow(fft_frame_s, clim = None, origin='lower', cmap=cmap_LTL, interpolation='none', extent = [r_axis[0], r_axis[-1], r_axis[0], r_axis[-1]]) #kx, ky, t
 #single_k_circle = plt.Circle((single_ky, single_kx), single_rad, color='red', linestyle = 'dashed', linewidth = 1.5, clip_on=False, fill=False)
 #ax[1].add_patch(single_k_circle)
-
-#im = ax[i].imshow(np.transpose(I[:,:,tMap-int(tint/2):tMap+int(tint/2), adc-1:adc+1].sum(axis=(2,3))), origin='lower', cmap='terrain_r', clim=None, interpolation='none', extent=[-2,2,-2,2]) #kx, ky, t
 ax[0].set_aspect(1)
 ax[1].set_aspect(1)
 ax[2].set_aspect(1)
@@ -362,10 +337,9 @@ ax[0].set_ylim(-2,2)
 ax[0].set_xlabel('$k_x$, $\AA^{-1}$', fontsize = 16)
 ax[0].set_ylabel('$k_y$,  $\AA^{-1}$', fontsize = 16)
 ax[0].tick_params(axis='both', labelsize=10)
-ax[0].set_title('$E$ = ' + str((tMaps[0])) + ' eV, ' + '$\Delta$E = ' + str(tint*dE) + ' eV', fontsize = 14)
-ax[0].set_title('$E$ = ' + str((tMaps[0])) + ' eV ', fontsize = 14)
-
-#fig.suptitle('E = ' + str(tMaps[0]) + ' eV, $\Delta$E = ' + str(tint_E) + ' meV,  $\Delta$$k_{rad}$ = ' + str(window_k_width) + ' $\AA^{-1}$', fontsize = 18)
+ax[0].set_title('$E$ = ' + str(E) + ' eV, ' + '$\Delta$E = ' + str(tint*dE) + ' eV', fontsize = 14)
+ax[0].set_title('$E$ = ' + str(E) + ' eV ', fontsize = 14)
+fig.suptitle('E = ' + str(E) + ' eV, $\Delta$E = ' + str(tint_E) + ' meV,  $\Delta$$k_{rad}$ = ' + str(window_k_width) + ' $\AA^{-1}$', fontsize = 18)
 
 ax[1].set_xlim(-2,2)
 ax[1].set_ylim(-2,2)
@@ -373,7 +347,6 @@ ax[1].set_ylim(-2,2)
 ax[1].set_xlabel('$k_x$, $\AA^{-1}$', fontsize = 16)
 ax[1].set_ylabel('$k_y$,  $\AA^{-1}$', fontsize = 16)
 ax[1].tick_params(axis='both', labelsize=10)
-#ax[1].set_title('$E$ = ' + str((tMaps[i])) + ' eV', fontsize = 16)
 ax[1].set_title('Windowed', fontsize = 15)
  
 ax[2].set_xlim(-2,2)
@@ -383,8 +356,6 @@ ax[2].set_xlabel('$r_x$, nm', fontsize = 16)
 ax[2].set_ylabel('$r_y$, nm', fontsize = 16)
 ax[2].tick_params(axis='both', labelsize=10)
 ax[2].set_title('2D FFT', fontsize = 15)
-#ax[2].set_title('$E$ = ' + str((tMaps[i])) + ' eV', fontsize = 16)
-#ax[i].annotate(('E = '+ str(round(tMaps[i],2)) + ' eV'), xy = (-1.85, 1.6), fontsize = 14, weight = 'bold')
 
 #ax[2].plot(r_axis, x_cut/np.max(1), color = 'black', label = '$r_b$')
 ax[3].plot(r_axis, x_cut/np.max(x_cut), color = 'black', label = '$r_x$')
@@ -421,7 +392,8 @@ new_rc_params = {'text.usetex': False,
 
 plt.rcParams.update(new_rc_params)
 
-fig.savefig(('MM_2DFFT' +'.svg'), format='svg')
+if save_figure is True:
+    fig.savefig((figure_file_name +'.svg'), format='svg')
     
 print(rdist_brad_x)
 print(rdist_brad_y)
