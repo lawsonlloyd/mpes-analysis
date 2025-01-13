dc2e4867f72bb6435350b23f713957afb0f2e0bc
Lawson Lloyd <51204777+lawsonlloyd@users.noreply.github.com>
2025-01-13 18:43:03 +0100

    Update SUPPORTING_ANALYSIS_trARPES.py

--- SUPPORTING_ANALYSIS_trARPES.py
+++ SUPPORTING_ANALYSIS_trARPES.py
@@ -16,14 +16,17 @@ from scipy.optimize import curve_fit
 
 #%%
 
-### ADDITIONAL SUPPORTING ANALYSIS
+### ADDITIONAL SUPPORTING ANALYSIS ###
+
 
 #%%
+
+%matplotlib inline
 
 # CALCULATING ABSORBED EXCITATION FLUENCE.
 
 thickness = 0.518e-9 #2e-9 # m, thickness of first layer (?)
-AOI = 40
+AOI = 35
 
 ### Load Dielectric Constant
 
@@ -46,9 +49,10 @@ c = 299792458 # m/s
 hbar = 6.58e-16  # eV.s
 
 ### Calculate Penetration Depth and Refractive Index
-k =  np.sqrt((np.sqrt((real_)**2 + (imag_)**2) - real_) / 2)
+k =  np.sqrt( (np.sqrt((real_)**2 + (imag_)**2) - real_) / 2)
 n = np.sqrt((np.sqrt((real_)**2 + (imag_)**2) + real_) / 2)
-pen_depth = np.sqrt(1-((np.sin(theta))**2/n**2))*(c*hbar/(2*k*energy))
+
+pen_depth = np.sqrt(1-((np.sin(theta))**2/n**2)) * (c*hbar/(2*k*energy))
 ABS = 1 - np.exp(-thickness/pen_depth)
 
 absorbance_2 = np.asarray([(1 - np.exp(-z*aa)) for aa in A_s_2])
@@ -59,7 +63,7 @@ penetration_depth = np.asarray(1/alpha)
 A_s_2 = 1/pen_depth
 pen_depth_2 = 1/A_s
 
-z = 2e-9
+z = .518e-9
 absorbance = alpha * z
 absorbance_2 = np.asarray([(1 - np.exp(-z*aa)) for aa in A_s_2])
 
@@ -69,7 +73,7 @@ plt.plot(energy, imag_, color = 'red', label = 'Imaginary')
 plt.plot(energy, real_, color = 'blue', label = 'Real')
 plt.ylabel('Int', color = 'purple')
 plt.xlabel('Energy (eV)')
-plt.xlim([1.3,2.4])
+plt.xlim([1.3,2.25])
 plt.ylim([-20,90])
 plt.title('Calculating Absorption')
 plt.legend(frameon=False)
@@ -77,13 +81,13 @@ plt.legend(frameon=False)
 ax2 = ax1.twinx()
 ax2.plot(energy, 100*ABS, color = 'pink', linestyle = 'dashed', label = 'Calc. Absorbance')
 plt.ylabel('% Abs', color = 'pink')
-plt.legend(frameon=False)
+plt.plot(energy, Abs_Huber,  color = 'purple', linestyle = 'dashed', label = 'Abs. Huber')
 
+plt.legend(frameon=False)
 #plt.plot(energy, 100*absorbance_2, color = 'black', linestyle = 'dashed', label = 'Calc. Absorbance')
 #plt.plot(energy, 100*absorbance, color = 'grey', linestyle = 'dashed', label = 'Absorbance')
 #plt.plot(energy, 100*A_s, color = 'pink', linestyle = 'dashed', label = 'A_s')
 #plt.plot(energy, 100*A_s_2, color = 'purple', linestyle = 'dashed', label = 'A_s')
-#plt.plot(energy, Abs_Huber,  color = 'green', linestyle = 'dashed', label = 'Abs. Huber')
 
 #%%
 
@@ -141,19 +145,96 @@ exc_density = (sum(neh_abs)/spot_size)/1e13
 
 ###
 fig, ax1 = plt.subplots()
-
-plt.plot(wl,spectrum_ph/np.max(spectrum_ph), color = 'red',label = 'Norm. Laser Spectrum')
+plt.plot(wl,abs_spec, label = 'Calc. Absorbance, %', color = 'black')
+plt.ylim([0, 0.06])
 plt.legend(frameon=False)
-plt.ylabel('Int., arb. un.')
+plt.ylabel('Abs.')
+plt.xlabel('Wavelength, nm')
 
 ax2 = ax1.twinx()
-ax2.plot(wl,abs_spec, label = 'Calc. Absorbance, %', color = 'black')
+ax2.plot(wl[2925-400:2925+400],spectrum_ph[2925-400:2925+400]/np.max(spectrum_ph), color = 'red',label = 'Norm. Laser Spectrum')
 #plt.plot(wl,abs_spec*spectrum_ph/spectrum_ph.max(), color = 'pink', linestyle = 'dashed', label = 'Effective Absorbed')
-plt.xlim([640,1000])
-plt.xlabel('Wavelength, nm')
-plt.ylabel('Int., arb. un.')
+plt.xlim([600, 1000])
+plt.ylim([0,1.05])
 plt.title('Estimated Carrier Density')
-plt.axvline(915, linestyle = 'dashed')
+plt.axvline(915, linestyle = 'dashed', color = 'pink')
 plt.legend(frameon=False)
-print('Estimated Carrier Density for ' + str(average_power) + ' mW, ' + str(round(energy_density, 2)) + ' mJ/cm2 : ' + str(round(exc_density, 2)) + ' E13/cm2')
+print('Estimated Carrier Density for ' + str(average_power) +  ' mW, ' + str(round(energy_density, 2)) + ' mJ/cm2 (' + str(lam) + 'nm) ' ': ' + str(round(exc_density, 2)) + ' E13/cm2')
 plt.show()
+
+#%%
+
+%matplotlib inline
+
+# CALCULATING ABSORBED EXCITATION FLUENCE: ALTERNATIVE METHOD
+
+thickness = 0.518e-9 #2e-9 # m, thickness of first layer (?)
+AOI = 35
+
+### Load Dielectric Constant
+
+real_epsilon =  np.loadtxt('Real_epsilon.txt')
+imag_epsilon = np.loadtxt('Imaginary_epsilon.txt')
+Abs_Huber_load = np.loadtxt('Abs_Huber2.txt')
+
+real_epsilon = np.transpose(real_epsilon)
+imag_epsilon = np.transpose(imag_epsilon)
+real_ = real_epsilon[1,:]
+imag_ = imag_epsilon[1,:]
+
+energy = np.asarray(imag_epsilon[0,:])
+
+Abs_Huber_load = np.transpose(Abs_Huber_load)
+Abs_Huber = np.interp(energy, Abs_Huber_load[0,:], Abs_Huber_load[1,:])
+
+theta = np.pi*AOI/180 #deg
+c = 299792458 # m/s
+hbar = 6.58e-16  # eV.s
+
+### Calculate Penetration Depth and Refractive Index
+k =  np.sqrt((np.sqrt((real_)**2 + (imag_)**2) - real_) / 2)
+n = np.sqrt((np.sqrt((real_)**2 + (imag_)**2) + real_) / 2)
+
+#
+absorbance_2 = np.asarray([(1 - np.exp(-z*aa)) for aa in A_s_2])
+
+nu_pump = energy/(4.1357e-15)
+alpha = 1/ (np.sqrt(real_ + imag_) * c * (1 / (2*np.pi*imag_*nu_pump)))
+#alpha = (2*energy)/(c*hbar) * np.sqrt((np.sqrt((real_)**2 + (imag_)**2) - real_) / 2) #1/m
+A_s = 4*n*np.cos(theta)/(n**2 + k**2+2*n*np.cos(theta)+(np.cos(theta))**2)
+
+penetration_depth = np.asarray(1/alpha)
+A_s_2 = 1/pen_depth
+pen_depth_2 = 1/A_s
+
+z = 150e-9
+absorbance = alpha * z
+absorbance_2 = np.asarray([(1 - np.exp(-z*aa)) for aa in A_s_2])
+
+
+absorbance = 1 - np.exp(-z*alpha)
+
+fig, ax1 = plt.subplots()
+
+plt.plot(energy, imag_, color = 'red', label = 'Imaginary')
+plt.plot(energy, real_, color = 'blue', label = 'Real')
+plt.ylabel('Int', color = 'purple')
+plt.xlabel('Energy (eV)')
+plt.xlim([1,2.25])
+plt.ylim([-20,90])
+plt.title('Calculating Absorption')
+plt.legend(frameon=False)
+
+ax2 = ax1.twinx()
+ax2.plot(energy, 100*absorbance, color = 'pink', linestyle = 'dashed', label = 'Calc. Absorbance')
+plt.ylabel('% Abs', color = 'pink')
+plt.legend(frameon=False)
+
+#plt.plot(energy, 100*absorbance_2, color = 'black', linestyle = 'dashed', label = 'Calc. Absorbance')
+#plt.plot(energy, 100*absorbance, color = 'grey', linestyle = 'dashed', label = 'Absorbance')
+#plt.plot(energy, 100*A_s, color = 'pink', linestyle = 'dashed', label = 'A_s')
+#plt.plot(energy, 100*A_s_2, color = 'purple', linestyle = 'dashed', label = 'A_s')
+#plt.plot(energy, Abs_Huber,  color = 'green', linestyle = 'dashed', label = 'Abs. Huber')
+
+#%%
+
